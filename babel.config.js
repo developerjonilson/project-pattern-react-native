86a00fb1a435bb471168e4734d71bb02134ca757
jonilsondeveloper <jonilsondeveloper@gmail.com>
2019-04-08 22:00:16 -0300

    inicio do modulo 2

---
+++ babel.config.js
@@ -0,0 +1,29 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+      [
+        'babel-plugin-root-import',
+        {
+          rootPathPrefix: '~',
+          rootPathSuffix: 'src',
+        },
+      ],
+    ],
+    env: {
+      // For React Native
+      production: {
+        plugins: [
+          [
+            'babel-plugin-root-import',
+            {
+              rootPathPrefix: '~',
+              rootPathSuffix: 'src',
+            },
+          ],
+        ],
+      },
+    },
+  };
+};
